5a214f2266919676692f8a910ab6ee760ebeaa7b
Nika Nozadze <nikanozadze1010@gmail.com>
2024-04-06 01:10:23 +0400

    feat: add image files & default colors

---
+++ src/svg/index.ts
@@ -0,0 +1,5 @@
+export { default as ArrowRight } from "./ArrowRight";
+export { default as CheckAll } from "./CheckAll";
+export { default as CheckCircle } from "./CheckCircle";
+export { default as ExclamationCircle } from "./ExclamationCircle";
+export { default as Success } from "./Success";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        error: "#DC3545",
+        bgRed: "#FD5334",
+        violet: "#7025FB",
+        "light-green": "#E9FAF1",
+      },
+    },
   },
   plugins: [],
-}
-
+};
